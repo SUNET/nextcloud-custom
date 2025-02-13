8d3f7376279d350c3859f6245870a8d4f44f21e2
Micke Nordin <kano@sunet.se>
2025-02-13 16:21:48 +0100

    Revert trick

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-(sleep 10s; su - www-data -s /bin/sh -c "php  /var/www/html/occ maintenance:update:htaccess") &
+su - www-data -s /bin/sh -c "php  /var/www/html/occ maintenance:update:htaccess"
 /usr/local/bin/docker-php-entrypoint "$@"
