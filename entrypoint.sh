4df624e387fc09905b46d9bf812add051bb02b30
Micke Nordin <kano@sunet.se>
2025-02-13 16:11:38 +0100

    Run occ after apache has started

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-su - www-data -s /bin/sh -c "php  /var/www/html/occ maintenance:update:htaccess"
+(sleep 10s; su - www-data -s /bin/sh -c "php  /var/www/html/occ maintenance:update:htaccess") &
 /usr/local/bin/docker-php-entrypoint "$@"
